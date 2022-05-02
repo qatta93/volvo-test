3ac0d91642f455f4cafb909209e7ac9c4f848c57
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-02 17:21:36 +0200

    fonts added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,14 +13,13 @@ import { ChevronSmall } from './ChevronSmall';
 export const Card = ({ car }:any) => {
   return (
     <article className={styles.carCard}>
-      <h2 className={styles.carCard__type}>{car.bodyType}</h2>
-      <h1 className={styles.carCard__model}>{car.modelName}{car.modelType}</h1>
+      <h2 className={styles.carCard__bodyType}>{car.bodyType}</h2>
+      <h1 className={styles.carCard__model}>{car.modelName}</h1>
+      <h1 className={styles.carCard__type}>{car.modelType}</h1>
       <img className={styles.carCard__img} src={car.imageUrl} alt="car" />
       <section className={styles.carCard__buttons}>
-        <a href={`/learn/${car.id}`}>LEARN</a>
-        <ChevronSmall />
-        <a href={`/shop/${car.id}`}>SHOP</a>
-        <ChevronSmall />
+        <a href={`/learn/${car.id}`}>LEARN <ChevronSmall /></a>
+        <a href={`/shop/${car.id}`}>SHOP <ChevronSmall /></a>
       </section>
     </article>
   )
--- src/components/ChevronSmall.tsx
+++ src/components/ChevronSmall.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ChevronSmall = () => {
   return (
-    <svg viewBox="0 0 11 11" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
       <path d="M2 1.5l4 4-4 4" fill="none" stroke="#1c6bba" stroke-width="1.5" stroke-linecap="round"
         stroke-linejoin="round"></path>
     </svg>
