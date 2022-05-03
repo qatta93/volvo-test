18ee69f033fb122b6c3239b3961c6e7da055e096
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-03 11:55:35 +0200

    pagination started

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,12 +1,33 @@
-import React, {useState} from "react";
+import React, { useState, useEffect }  from "react";
 import { Card } from "./Card";
-import cars from '../../public/api/cars.json'
-import styles from '../../public/css/Slider.module.css'
-import logo from '../../docs/chevron-small.svg';
+import cars from '../../public/api/cars.json';
+import styles from '../../public/css/Slider.module.css';
+import { useMediaQuery } from 'react-responsive';
+import { ChevronCircledLeft, ChevronCircledRight } from "./ChevronCircled";
 
 export const Slider: React.FC = () => {
-  console.log(logo)
+  const isDesktop = useMediaQuery({ query: '(min-device-width: 1024px)' });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [carsPerPage, setCarsPerPage] = useState(4);
 
+  const indexOfLastCar = currentPage * carsPerPage;
+  const indexOfFirstCar = indexOfLastCar - carsPerPage;
+  const currentCars = cars.slice(indexOfFirstCar, indexOfLastCar);
+  console.log(currentCars);
+
+  if (isDesktop) {
+    return (
+      <main className={styles.carsDesktop}>
+        <section className={styles.cars__containerDesktop}>
+          {cars.map((car) => <Card car={currentCars} />)}
+        </section>
+        <section className={styles.cars__navDesktop}>
+          <ChevronCircledLeft />
+          <ChevronCircledRight />
+        </section>
+      </main>
+    );
+  }
   return (
     <main className={styles.cars}>
       <section className={styles.cars__container}>
