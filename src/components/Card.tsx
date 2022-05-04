91b16ceb163c866b4cbc1ef5ca524106af97b358
qatta93 <panasiuk.patrycja@gmail.com>
2022-05-04 11:23:21 +0200

    rwd done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,8 +18,10 @@ export const Card = ({ car }:CarProps) => {
   return (
     <article className={styles.carCard}>
       <h2 className={styles.carCard__bodyType}>{car.bodyType}</h2>
-      <h1 className={styles.carCard__model}>{car.modelName}</h1>
-      <h1 className={styles.carCard__type}>{car.modelType}</h1>
+      <div className={styles.carCard__container}>
+        <h1 className={styles.carCard__model}>{car.modelName}</h1>
+        <h1 className={styles.carCard__type}>{car.modelType}</h1>
+      </div>
       <img className={styles.carCard__img} src={car.imageUrl} alt="car" />
       <section className={styles.carCard__buttons}>
         <a className={styles.carCard__btn} href={`/learn/${car.id}`}>
